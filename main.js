0b2b86cf204e9d8636bb890a452ec8577412dfa0
m1so2 <0cu5rz4m38d260c@au.com>
2024-09-19 05:18:34 +0000

    9_19 JavaScriptの導入

---
+++ main.js
@@ -0,0 +1,14 @@
+document.querySelector("h1").textContent = "自分の名前"
+document.querySelector("h2").textContent = "河野樹道"
+
+//docmentとは！！
+/*htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。*/
+
+//DOM(ドム)
+/*ドキュメントオブジェクトモデル(Document Object Model)の略で、webページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+
+//ドキュメントの中の("h1")タグを探してね
+//docment.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent = "河野樹道"
