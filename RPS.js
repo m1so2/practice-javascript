237a828a8391388c2fbeb1af171b145dafcde533
m1so2 <0cu5rz4m38d260c@au.com>
2024-11-22 06:14:37 +0000

    11_22

--- RPS.js
+++ RPS.js
@@ -1,36 +1,62 @@
 function RPS(){
-    const choices = ["gu","choki","pa","muteki"];
-
-    // let select = document.createElement("select");
-    // let option1 = document.createElement("option");
-    // let option2 = document.createElement("option");
-    // let option3 = document.createElement("option");
-    // const gu = "gu";
-    // option1.value = gu;
-    // option1.textContent = gu;
-    // select.appendChild(option1);
-    // option2.value = choki;
-    // option2.textContent = choki;
-    // select.appendChild(option2);
-    // option3.value = pa;
-    // option3.textContent = pa;
-    // select.appendChild(option3);
-    // gameContainer.appendChild(select);
+    const choices = ["gu","choki","pa"];
 
     let select = document.createElement("select");
+    select.id = "choices"
     choices.forEach(choice => {
         let option = document.createElement("option");
         option.value = choice;
         option.textContent = choice;
+        option.id = choice;
         select.appendChild(option);
     });
     gameContainer.appendChild(select);
 
-    let image = document.createElement("img");
-    image.alt = "Choice";
-    image.style.width = "100px";
-    image.style.height = "100px";
-    image.src = "https://www.free-materials.com/adm/wp-content/uploads/2022/10/0adpTSC_0119-.jpg";
+    let choiceButton = document.createElement("button")
+    choiceButton.textContent = "決定"
 
+    gameContainer.appendChild(choiceButton);
+
+    let image = document.createElement("img");
+    image.alt = "choice";
+    image.style.width = "200px";
+    image.style.height = "200px";
+ 
+    let enemyImage = document.createElement("img");
+    enemyImage.alt = "choice";
+    enemyImage.style.width = "200px";
+    enemyImage.style.height = "200px";
+
+        choiceButton.addEventListener("click",function(){
+            switch(select.value){
+                case "gu":
+                    image.src = "https://asset.recipe-blog.jp/c/sig=1.Ja8Ih3Z9TOzxqisI-4BlNVa-W0MiZjsCig757bJey9A=,a=2,w=640,h=640/item/55/a9/0087264203ecdae41d744b4db862d62dc45355a9.jpg"
+                    break;
+                case "choki":
+                    image.src = "https://user0514.cdnw.net/shared/img/thumb/DANDA170629sp_TP_V.jpg"
+                    break;
+                case "pa":
+                    image.src = "https://www.suruga-ya.jp/pics_light/boxart_m/gg344518m.jpg"
+                    break;
+                default:
+                    break;
+            }
+            let enemyHand = choices[Math.floor(Math.random() *choices.length )];
+
+            switch(enemyHand){
+                case "gu":
+                    enemyImage.src = "https://asset.recipe-blog.jp/c/sig=1.Ja8Ih3Z9TOzxqisI-4BlNVa-W0MiZjsCig757bJey9A=,a=2,w=640,h=640/item/55/a9/0087264203ecdae41d744b4db862d62dc45355a9.jpg"
+                    break;
+                case "choki":
+                    enemyImage.src = "https://user0514.cdnw.net/shared/img/thumb/DANDA170629sp_TP_V.jpg"
+                    break;
+                case "pa":
+                    enemyImage.src = "https://www.suruga-ya.jp/pics_light/boxart_m/gg344518m.jpg"
+                    break;
+                default:
+                    break;
+            }
+    })
     gameContainer.appendChild(image);
+    gameContainer.appendChild(enemyImage);
 }
