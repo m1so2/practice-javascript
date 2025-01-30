de374040a047e7db9cd84ce530845f28db61c39d
m1so2 <0cu5rz4m38d260c@au.com>
2025-01-30 05:14:14 +0000

    カウントの追加

--- main.js
+++ main.js
@@ -63,6 +63,11 @@ function startNumberGuessGame(){
         })
 
         gameContainer.appendChild(button);
+
+        let count = 0;
+        const countDisplay = document.createElement("p")
+        countDisplay.textContent = `試行回数:${count}`;
+        gameContainer.appendChild(countDisplay);
 }
 
 startNumberGuessGame();
