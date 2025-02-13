8d6a178d0d9ab5ed2e8a2247172fc515e5df023d
m1so2 <0cu5rz4m38d260c@au.com>
2025-02-13 05:21:59 +0000

    2_13

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 const gameSelector = document.getElementById('game-select');
-const gameContainer = document.getElementById("game-container");
+const select = document.createElement("select");
+
+    select.name = "games";
+    select.id = "game-select";
+    const options = [
+        { value: "",text:"--ゲームを選択してください--"},
+        { value: "clicker",text: "クリッカーゲーム"},
+        { value: "number-guess",text:"数当てゲーム"}
+    ];
+    options.forEach(optionData =>{
+        const option = document.createElement("option");
+        option.value = optionData.value;
+        option.textContent = optionData.text;
+        select.appendChild(option);
+    });
+gameSelector.appendChild(select);
+
+
+
 gameSelector.addEventListener('change',function(){
     gameContainer.innerHTML = '';
     switch (gameSelector.value){
